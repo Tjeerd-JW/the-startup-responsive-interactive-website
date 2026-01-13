f1f93d6a7d44c46707d82721bd0b93bb73d1243c
Tjeerd <tj.wokke@gmail.com>
2026-01-13 12:11:59 +0100

    forecast table #17

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.modal button')
-const showModal = document.querySelector('.activities')
+const openButtons = document.querySelectorAll('.modal-button')
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('open')
+        modal.classList.add('modal-show')
+    });
+});
 
 closeButton.addEventListener("click", closeModal)
-showModal.addEventListener("click", openModal)
 
 function closeModal() {
     console.log('sluiten')
     modal.classList.remove('modal-show')
-}
-function openModal() {
-    console.log('open')
-    modal.classList.add('modal-show')
-
 }
