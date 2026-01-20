de9c01e6821b730029c34b217ddac50628b24364
Tjeerd <tj.wokke@gmail.com>
2026-01-20 11:30:50 +0100

    mail forecast click animation #19

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.modal button')
 const openButtons = document.querySelectorAll('.modal-button')
+const mailButton = document.querySelector('.forecast button')
 
 openButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -16,6 +17,16 @@ function closeModal() {
     modal.classList.remove('modal-show')
     modal.classList.add('modal-not-show')
 }
-modal.addEventListener("animationend",() =>{
+modal.addEventListener("animationend", () => {
     modal.classList.remove('modal-not-show')
+})
+
+mailButton.addEventListener("click", mailHandler)
+
+function mailHandler() {
+    mailButton.classList.add('mail-submit')
+}
+
+mailButton.addEventListener("animationend", () =>{
+    mailButton.classList.remove('mail-submit')
 })
